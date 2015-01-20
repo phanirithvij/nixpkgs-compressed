9ac67e9e31127a438111dc19dfc6693ebe366fa3
Ricardo M. Correia <rcorreia@wizy.org>
2015-01-20 01:10:25 +0100

    pythonPackages.scikitlearn: Disable checks
    test_disk_used can fail when building on a ZFS filesystem with
    compression enabled.
    
    See scikit-learn/scikit-learn I-4127 for more information.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -23321,6 +23321,9 @@ in {
       HOME=$TMPDIR OMP_NUM_THREADS=1 nosetests $out/${python.sitePackages}/sklearn/
     '';
 
+    # test_disk_used can fail (see scikit-learn/scikit-learn#4127)
+    doCheck = false;
+
     meta = {
       description = "A set of python modules for machine learning and data mining";
       homepage = http://scikit-learn.org;
