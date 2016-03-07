2410211f8acc43978d7cdba10056300f5ea126f1
Ricardo M. Correia <rcorreia@wizy.org>
2016-03-07 17:10:42 +0100

    Revert "libvirt: add xen support"
    This reverts commit 4e225b61d250100adcb46861788bef7d908baeeb.

--- pkgs/development/libraries/libvirt/default.nix
+++ pkgs/development/libraries/libvirt/default.nix
@@ -4,7 +4,7 @@
 , iproute, iptables, readline, lvm2, utillinux, systemd, libpciaccess, gettext
 , libtasn1, ebtables, libgcrypt, yajl, pmutils, libcap_ng, libapparmor
 , dnsmasq, libnl, libpcap, libxslt, xhtml1, numad, numactl, perlPackages
-, curl, libiconv, gmp, xen, zfs
+, curl, libiconv, gmp, zfs
 }:
 
 with stdenv.lib;
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
     attr libxslt xhtml1 perlPackages.XMLXPath curl libpcap
   ] ++ optionals stdenv.isLinux [
     libpciaccess devicemapper lvm2 utillinux systemd libnl numad zfs
-    libapparmor libcap_ng numactl xen
+    libapparmor libcap_ng numactl
   ] ++ optionals stdenv.isDarwin [
      libiconv gmp
   ];
